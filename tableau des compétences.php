ece8e159470824c00f050e09b0deab110be77766
Aurelie <aubin200683@gmail.com>
2025-11-09 18:41:05 +0100

    tableau 1

---
+++ tableau des compétences.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+// Tableau des compétences -
+   
+ Gerer patrimoine informatique
+"▸Recenser et identifier les ressources numériques 
+▸Exploiter des référentiels, normes et standards adoptés par le prestataire informatique
+▸Mettre en place et vérifier les niveaux d’habilitation associés à un service
+▸Vérifier les conditions de la continuité d’un service informatique
+▸Gérer des sauvegardes
+▸Vérifier le respect des règles d’utilisation des ressources numériques"
+
+Répondre aux incidents et aux demandes d’assistance et d’évolution
+"▸Collecter, suivre et orienter des demandes 
+▸Traiter des demandes concernant les services réseau et système, applicatifs
+▸Traiter des demandes concernant les applications"
+
+Développer la présence en ligne de l’organisation
+"▸Participer à la valorisation de l’image de l’organisation sur les médias numériques en tenant compte du cadre juridique et des enjeux économiques
+▸Référencer les services en ligne de l’organisation et mesurer leur visibilité.
+▸Participer à l’évolution d’un site Web exploitant les données de l’organisation."
+
+Mettre à disposition des utilisateurs un service informatique
+"▸Réaliser les tests d’intégration et d’acceptation d’un service
+▸Déployer un service
+▸Accompagner les utilisateurs dans la mise en place d’un service"
+
+
+
+?>
